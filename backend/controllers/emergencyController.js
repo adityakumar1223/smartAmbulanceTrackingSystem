c5cb2038d3b84acca0c40702087136d29ef1aa1d
Aditya Kumar <adityakumarak1223@gmail.com>
2026-05-18 20:39:21 +0530

    Updated routes for emergency controller.

--- backend/controllers/emergencyController.js
+++ backend/controllers/emergencyController.js
@@ -26,4 +26,168 @@ const createEmergencyRequest = async (req, res) => {
     }
 };
 
-module.exports = {createEmergencyRequest };
+const getAllEmergencyRequest = async (req, res) =>{
+
+
+    try {
+
+        const requests = await emergencyRequest.find()
+            .populate("patientId","name email role")
+            .populate("assignedDriver", "name email");
+
+            res.status(200).json({
+                requests,
+            })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+    
+    
+}
+
+const getAllPendingRequest = async(req, res) => {
+
+    try {
+        const requests = await emergencyRequest.find({
+            status: "pending",
+        });
+
+        res.status(200).json({
+            requests
+        });
+
+    } catch (error) {
+
+        res.send(500).json({
+            message: error.message,
+        });
+        
+    }
+    
+};
+
+const acceptEmergencyRequest = async (req, res) => {
+
+    try {
+
+        const requestId = req.params.id;
+        const request = await emergencyRequest.findById(requestId);
+
+        if(!requestId){
+            res.send(404).json({
+                message: "Emergency request not found",
+            });
+        }
+
+        //preventing accepting already accepted requests
+        if(request.status !== "pending"){
+            res.status(200).json({
+                message: "Request already accepted",
+            });
+        };
+
+        request.status = "accepted";
+
+        request.assignedDriver = req.user.id;
+
+        await request.save();
+
+        res.status(200).json({
+            message: "Emergency request accepted",
+            request,
+        })
+        
+    } catch (error) {
+        console.log("bounced");
+        res.status(500).json({
+            
+            message: error.message,
+        });
+    }
+
+}
+
+const updateEmergencyStatus = async (req, res) =>{
+
+    const validStatuses = [
+        "pending",
+        "accepted",
+        "on_the_way",
+        "arrived",
+        "completed"
+    ];
+
+    try {
+        
+        const requestId = req.params.id;
+
+        const {status} = req.body;
+
+        const request = await emergencyRequest.findById(requestId);
+
+        if(!requestId) {
+            return res.status(400).json({
+                message: "Emergency request not found"
+            })
+        }
+
+        if(!validStatuses.includes(status)){
+            return res.status(200).json({
+                message: "Invalid status",
+            });
+        };
+
+        request.status = status;
+
+        await request.save();
+
+        res.status(200).json({
+            message: "Emergency status updated"
+        });
+
+
+
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+const getDriverRequest = async (req, res) => {
+
+    try {
+
+        const requests = await emergencyRequest.find({
+            assignedDriver: req.user.id,
+        })
+
+        .populate("patientId", "name email")
+        .sort({createdAt: -1});
+
+        res.status(200).json({
+            requests,
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+    
+};
+
+
+
+module.exports = {
+    createEmergencyRequest, 
+    getAllEmergencyRequest, 
+    acceptEmergencyRequest, 
+    getAllPendingRequest, 
+    updateEmergencyStatus,
+    getDriverRequest
+};
--- backend/routes/emergencyRoutes.js
+++ backend/routes/emergencyRoutes.js
@@ -3,10 +3,37 @@ const router = express.Router();
 
 const {protect, authorizeRoles, } = require("../middleware/authMiddleware.js");
 
-const {createEmergencyRequest} = require("../controllers/emergencyController.js");
+const {
+    createEmergencyRequest, 
+    getAllEmergencyRequest, 
+    acceptEmergencyRequest, 
+    getAllPendingRequest, 
+    updateEmergencyStatus,
+    getDriverRequest
+                            } = require("../controllers/emergencyController.js");
 
 //patient creates request
 
 router.post("/request", protect, authorizeRoles("patient"), createEmergencyRequest);
+
+//hospital and admin can get all the emergency requests
+
+router.get("/all", protect, authorizeRoles("admin", "hospital"), getAllEmergencyRequest);
+
+//driver accepts the emergency request
+
+router.put("/accept/:id", protect, authorizeRoles("driver"), acceptEmergencyRequest);
+
+// drivers to see pending requests
+
+router.get("/pending", protect, authorizeRoles("driver"), getAllPendingRequest);
+
+//updating status of the emergency request
+
+router.put("/updateStatus/:id", protect, authorizeRoles("driver"), updateEmergencyStatus);
+
+//driver can see their requests
+
+router.get("/my-requests", protect, authorizeRoles("driver"), getDriverRequest);
 
 module.exports = router;
